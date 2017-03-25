dc7c41b9141f1ba0f64707f591783e6099331b68
Charles-K-Temple <tug26390@temple.edu>
2017-03-25 18:02:30 -0400

    Git Init

---
+++ Grp2Mastermind/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Grp2Mastermind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 768);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn0.Location = new System.Drawing.Point(293, 629);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(45, 45);
+            this.btn0.TabIndex = 3;
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(287, 29);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(45, 46);
+            this.pic1.TabIndex = 4;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(344, 29);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(45, 46);
+            this.pic2.TabIndex = 5;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic2_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(402, 29);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(45, 46);
+            this.pic3.TabIndex = 6;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic3_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(459, 29);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(45, 46);
+            this.pic4.TabIndex = 7;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 790);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+    }
+}
+
---
+++ Grp2Mastermind/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grp2Mastermind
+{
+    public partial class Form1 : Form
+    {
+        // Seed the RNG.
+        Random rng = new Random();
+        string[] colors = new string[] { "blank", "Red", "Blue", "Green", "Yellow", "White", "Black", "Orange", "Violet" };
+        int[] computerPix = new int[4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SEED = rng.Next(13, 137);
+            computerPix = GenerateNonRepeatingNumbers(SEED, 1, 8);
+            string str = "";
+            for (int i = 0; i < 4; i++)
+            {
+                str += "  :  ";
+                str += computerPix[i].ToString();
+            }
+            textBox1.Text = str;
+        }
+
+        /*
+         * The following is mostly lifted from
+         * http://stackoverflow.com/questions/22737687/how-to-create-an-array-of-non-repeating-random-numbers
+         * with some design changes to suit our needs
+         * 
+         * */
+        public int[] GenerateNonRepeatingNumbers(int seed, int min, int range)
+        {
+            // Make sure range is an appropriate value
+            if (range <= 0)
+            {
+                throw new ArgumentException("Range must be greater than zero.");
+            }
+
+            // Make an array to hold our numbers
+            int[] numbers = new int[range];
+
+            // Seed the RNG.
+            // Random rng = new Random(seed);
+
+            // Fill the array with all numbers from min to min + range
+            for (int i = 0; i < range; numbers[i] = min + i++) { }
+
+            int
+                a = 0, // Swap index
+                t = 0; // Temporary value storage
+
+            // Scramble the values
+            for (int i = 0; i < range; i++)
+            {
+                // Get a random index that isn't i
+                while ((a = rng.Next(range)) == i) { };
+                // Store the old value at i
+                t = numbers[i];
+                // Change the old value to the value at the random index
+                numbers[i] = numbers[a];
+                // Set value at random index to our old value from numbers[i]
+                numbers[a] = t;
+            }
+
+            return numbers;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            string colorX = "Red";
+            //Color = Color.FromName.
+            btn0.Visible = true;
+            btn0.BackColor = Color.FromName(colorX);
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+           pic1.BackColor = Color.FromName(colors[computerPix[0]]);
+        }
+
+        private void pic2_Click(object sender, EventArgs e)
+        {
+            pic2.BackColor = Color.FromName(colors[computerPix[1]]);
+        }
+
+        private void pic3_Click(object sender, EventArgs e)
+        {
+            pic3.BackColor = Color.FromName(colors[computerPix[2]]);
+        }
+
+        private void pic4_Click(object sender, EventArgs e)
+        {
+            pic4.BackColor = Color.FromName(colors[computerPix[3]]);
+        }
+    }
+}
